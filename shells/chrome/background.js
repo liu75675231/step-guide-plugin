680191fb49af5de19c27d8e8022b18e8a90476e9
liu haitao <liu75675231@126.com>
2019-08-20 18:34:37 +0800

    点击弹出页面

--- shells/chrome/background.js
+++ shells/chrome/background.js
@@ -1,5 +1,37 @@
+var windowId = undefined;
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.tabs.executeScript({
-        file: 'contentScript.js'
+//    chrome.tabs.executeScript({
+//        file: 'contentScript.js'
+//    });
+
+    chrome.browserAction.onClicked.addListener(function callback () {
+
+        console.log(windowId)
+        if (windowId) {
+            console.log(chrome.windows.get(windowId, {},function (window) {
+                if (!window) {
+                    createWindow();
+                } else {
+                    if (!window.focused) {
+                        chrome.windows.update(windowId, {focused: true}, function callback() {
+
+                        });
+                    }
+                }
+            }));
+        } else {
+            createWindow();
+        }
+
+
     });
 });
+
+function createWindow () {
+    chrome.windows.getAll(function (windows) {
+        chrome.windows.create({'url': 'index.html', 'type': 'popup'}, function(window) {
+            console.log('sdfsdf');
+            windowId = window.id;
+        });
+    });
+}
