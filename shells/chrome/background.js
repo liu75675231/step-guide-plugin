16477f3955925afaa1caf0f18ac1e59e49f6b342
liu haitao <liu75675231@126.com>
2019-08-21 18:14:28 +0800

    update

--- shells/chrome/background.js
+++ shells/chrome/background.js
@@ -1,37 +1,25 @@
-var windowId = undefined;
+
 chrome.runtime.onInstalled.addListener(function() {
 //    chrome.tabs.executeScript({
 //        file: 'contentScript.js'
 //    });
 
-    chrome.browserAction.onClicked.addListener(function callback () {
-
-        console.log(windowId)
-        if (windowId) {
-            console.log(chrome.windows.get(windowId, {},function (window) {
-                if (!window) {
-                    createWindow();
-                } else {
-                    if (!window.focused) {
-                        chrome.windows.update(windowId, {focused: true}, function callback() {
-
-                        });
-                    }
-                }
-            }));
-        } else {
-            createWindow();
-        }
-
-
-    });
 });
 
-function createWindow () {
-    chrome.windows.getAll(function (windows) {
-        chrome.windows.create({'url': 'index.html', 'type': 'popup'}, function(window) {
-            console.log('sdfsdf');
-            windowId = window.id;
+var curWindowId = undefined;
+chrome.browserAction.onClicked.addListener(function callback () {
+    if (!curWindowId) {
+        chrome.windows.getAll(function (windows) {
+            chrome.windows.create({'url': 'index.html', 'type': 'popup'}, function(window) {
+                console.log('sdfsdf');
+                curWindowId = window.id;
+            });
         });
-    });
-}
+    }
+});
+
+chrome.windows.onRemoved.addListener(function callback (windowId) {
+    if (windowId === curWindowId) {
+        curWindowId = undefined;
+    }
+})
